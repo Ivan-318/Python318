6e41f5649cfd17b44c806d9fb48f28659b52d23b
Ivan <ivan1991_sidorow@mail.ru>
2024-07-14 18:29:39 +0900

    errordb.py

---
+++ two/dbsite.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, flash
+import sqlite3
+import os
+
+DEBUG = True
+SECRET_KEY = 'c5e4adf5aba5dcaf09f66c73eadf1b153cfc1ee2'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsk.db')))
+
+
+def connect_db():
+    con = sqlite3.connect(app.config['DATABASE'])
+    con.row_factory = sqlite3.Row
+    return con
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', "r") as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
