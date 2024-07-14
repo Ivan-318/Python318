6af183383a4c28d0fdf3d42832903ee40d38d3f0
Ivan <ivan1991_sidorow@mail.ru>
2024-07-15 08:22:52 +0900

    rabdbsite.py

--- two/dbsite.py
+++ two/dbsite.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, g
 import sqlite3
 import os
+from FDataBase import FDataBase
 
 DEBUG = True
 SECRET_KEY = 'c5e4adf5aba5dcaf09f66c73eadf1b153cfc1ee2'
@@ -23,5 +24,36 @@ def create_db():
         db.cursor().executescript(f.read())
     db.commit()
     db.close()
+
+
+def get_db():
+    if not hasattr(g, 'link.db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.route("/")
+def index():
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('index.html', menu=dbase.get_menu())
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link.db'):
+        g.link_db.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('page404.html', title="Страница не найдена", menu=dbase.get_menu())
+
+
+if __name__ == '__main__':
+    app.run()
+
 
 
