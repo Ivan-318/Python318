952aac923fac730b89f2b32c8dfb8f9d30ac6fac
Ivan <ivan1991_sidorow@mail.ru>
2024-07-31 15:51:49 +0900

    dz49get_titles.py

--- dz/dz48/main_portfolio/blog/models.py
+++ dz/dz48/main_portfolio/blog/models.py
@@ -5,7 +5,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
     date = models.DateField()
-    # pdf = models.FileField(upload_to='skills/pdf/')
+    pdf = models.FileField(upload_to='skills/pdf/')
 
     def __str__(self):
         return self.title
