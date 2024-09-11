acdc32a82b1ba290c23e883cfa3eff4d828097c9
Ivan <ivan1991_sidorow@mail.ru>
2024-09-11 21:26:52 +0900

    dz54.py

--- dz/fourth/devsearch/users/urls.py
+++ dz/fourth/devsearch/users/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('profile/<str:pk>/', views.user_profile, name="user_profile"),
     path('login/', views.login_user, name="login"),
     path('logout/', views.logout_user, name="logout"),
+    path('register/', views.register_user, name="register"),
+    path('account/', views.user_account, name="account"),
+    path('edit-account/', views.edit_account, name="edit-account"),
 ]
