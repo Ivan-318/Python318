cc90dc7a856fe49d2b26497d7f6a34d0133b9c11
Ivan <ivan1991_sidorow@mail.ru>
2024-09-29 17:50:41 +0900

    zan62.py

--- dz/fifth/programming_blog/blog/views.py
+++ dz/fifth/programming_blog/blog/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from .models import *
 from .forms import *
 from django.urls import reverse_lazy
 from .utils import *
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
 
 
 class BlogHome(DataMixin, ListView):
-    model = Blog    # posts = Blog.objects.all()
+    # paginate_by = 3
+    model = Blog  # posts = Blog.objects.all()
     template_name = "blog/index.html"
     context_object_name = "posts"
 
@@ -38,6 +43,7 @@ class ShowPost(DataMixin, DetailView):
 
 
 class BlogCategory(DataMixin, ListView):
+    # paginate_by = 3
     model = Blog
     template_name = "blog/index.html"
     context_object_name = "posts"
@@ -55,14 +61,41 @@ class BlogCategory(DataMixin, ListView):
         return dict(list(context.items()) + list(c_def.items()))
 
 
-class AddPage(DataMixin, CreateView):
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'blog/addpage.html'
     success_url = reverse_lazy('index')
+    login_url = reverse_lazy('login')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         # context['title'] = "Добавление статьи"
         # context['menu'] = menu
         c_def = self.get_user_context(title="Добавление статьи")
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'blog/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('index')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = AuthenticationForm
+    template_name = 'blog/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
         return dict(list(context.items()) + list(c_def.items()))
